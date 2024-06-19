6ced9bc95930bda58a4a53d72ed6071c83af88bc
pat wall <patgwall@iu.edu>
2024-06-19 17:05:08 -0400

    graph tests passinggit statusgit status

--- src/neural_network/graph.rs
+++ src/neural_network/graph.rs
@@ -37,14 +37,14 @@ impl fmt::Display for PathingError {
 impl error::Error for PathingError {}
 
 /// The Graph struct contains structural information about a network without the details needed to
-/// fully compute activation. 
-/// 
+/// fully compute activation.
+///
 /// We use a Graph primarily to determine the activation order of nodes
 /// in our neural network. It can also be used to compute other general graph structural properties.
 /// In the current implementation sensors and outputs are owned by this struct which works fine.
 /// However, this leads to extra copying elsewhere and I only only made this choice to get my tests
-/// running. 
-/// 
+/// running.
+///
 /// In the actual code, lifetime annotations ought to be sufficient to ensure valid
 /// references to sensors and outputs from the neural network information.
 pub struct Graph {
@@ -56,7 +56,6 @@ pub struct Graph {
 }
 
 impl Graph {
-
     /// Default constructor for Graph that produces an empty graph.
     pub fn new() -> Graph {
         Graph {
@@ -70,9 +69,9 @@ impl Graph {
 
     /// This is a more useful constructor for graph that takes an edge list and produces a fully
     /// populated Graph object.
-    /// 
+    ///
     /// # Example
-    /// 
+    ///
     /// ```
     /// let edges = vec![[0, 2], [0, 1], [1, 2]];
     /// let my_graph = Graph::from_edge_list(edges, vec![0], vec![2]);
@@ -95,9 +94,9 @@ impl Graph {
     }
 
     /// Produces a Hash lookup for neighbors of a node looking either forward or backward.
-    /// 
+    ///
     /// # Example
-    /// 
+    ///
     /// ```
     /// let edges = vec![[0, 1], [1, 2], [0, 2]];
     /// let preds = Graph::dependency_map(&edges, True);
@@ -114,7 +113,7 @@ impl Graph {
             let val_node: usize;
 
             // want to ensure that all nodes are keys in the map regardless
-            
+
             match backward {
                 true => {
                     key_node = edge[1];
@@ -125,24 +124,25 @@ impl Graph {
                     val_node = edge[1];
                 }
             };
-            
+
             // add key if its not there already
-            dep_map.entry(key_node)
-                   .or_default()  // if key is not present add the default empty HashSet we're checking this twice now
-                   .insert(val_node);  // if it exists add the value to the HashSet
-            
+            dep_map
+                .entry(key_node)
+                .or_default() // if key is not present add the default empty HashSet we're checking this twice now
+                .insert(val_node); // if it exists add the value to the HashSet
+
             // also make sure the other node exists as key
             dep_map.entry(val_node).or_default();
-    }
+        }
 
         dep_map
     }
 
     /// Calculates an ordering of node innovation numbers according to the topological sort of the
     /// graph. If the graph is not a DAG this function throws CycleError.
-    /// 
+    ///
     ///  # Example
-    /// 
+    ///
     /// ```
     /// let edges = vec![[0, 1], [1, 2], [0, 2]];
     /// let g = Graph::from_edge_list(edges, 0, 2);
@@ -156,9 +156,8 @@ impl Graph {
         }
 
         let mut nodes = Vec::new();
-        
-        for node in ts {
 
+        for node in ts {
             match node {
                 Ok((node, _)) => nodes.push(*node),
                 Err(_) => return Err(topo_sort::CycleError),
@@ -168,18 +167,18 @@ impl Graph {
         Ok(nodes)
     }
 
-    /// Determines the activation order for an arbitrary neural network. 
-    /// 
+    /// Determines the activation order for an arbitrary neural network.
+    ///
     /// This will either return a
     /// a valid sorting withour reference to sensors and outputs for DAG or return an invalid but
-    /// useable ordering from the perspective of connecting sensors to outputs if cycles are 
-    /// present. 
-    /// 
+    /// useable ordering from the perspective of connecting sensors to outputs if cycles are
+    /// present.
+    ///
     /// If the sensors are disconnected from the outputs this function throws a
     /// ConnectivityError.
-    /// 
+    ///
     /// # Example
-    /// 
+    ///
     /// ```
     /// let edges = vec![[0, 1], [0, 2], [1, 2]];
     /// let g = Graph::from_edge_list(edges, vec![0], vec![2]);
@@ -200,10 +199,8 @@ impl Graph {
         // we'll iterate over the map checking that the node can be placed into the vector resizing
         // the vector as needed (slow probably)
         for &node in layer_map.keys() {
-
             // add the node to the inner vector corresponding to the mapped layer
             layer_vec[layer_map[&node]].push(node);
-        
         }
 
         // flatten into an ordering
@@ -214,13 +211,12 @@ impl Graph {
         }
 
         Ok(partial_order.to_owned())
-
     }
 
     /// This function returns the shortest path length from any sensor to the target node.
-    /// 
+    ///
     /// # Example
-    /// 
+    ///
     /// ```
     /// let edges = vec![[0, 2], [1, 2], [1, 3], [2, 3]];
     /// let g = Graph::from_edge_list(edges, vec![0, 1], vec![3]);
@@ -228,8 +224,8 @@ impl Graph {
     /// ```
     pub fn get_node_depth(&self, target_node: &usize) -> Result<usize, ConnectivityError> {
         let mut lengths: Vec<usize> = Vec::with_capacity(self.sensors.len());
-        let mut num_errors = 0;  // ensures that we error out if all sensors fail to connect
-        let mut depth: usize = 20;  // not sure why this is 20. i guess just arbitrary big number?
+        let mut num_errors = 0; // ensures that we error out if all sensors fail to connect
+        let mut depth: usize = 20; // not sure why this is 20. i guess just arbitrary big number?
 
         // get all the path lengths and count unreachable inputs
         for sensor in &self.sensors {
@@ -309,7 +305,6 @@ impl Graph {
     /// Each key of the returned map is the innovation number of a node and its corresponding value
     /// is the layer to which is belongs. This function should maybe be broken up.
     pub fn get_layer_map(&self) -> Result<HashMap<usize, usize>, ConnectivityError> {
-
         // holds things we need to keep track of in bfs
         let mut rev_layers: HashMap<usize, usize> = HashMap::with_capacity(self.preds.len());
         let mut layer = 0;
@@ -328,34 +323,24 @@ impl Graph {
             let curr = &queue.pop_front().unwrap();
             let mut next_queue = VecDeque::new();
 
-
             // here we're doing some checks to seperate nodes with shared output to diff layers
-            // we do this before the normal breadth-first searching because we might have to move
-            // the current node or that a node in the queue shouldn't properly be in this queue
-            // and should instead be in the next
             for node in &queue {
-                
                 let node_preceeds_current = self.preds[curr].contains(node);
                 let current_preceeds_node = self.preds[node].contains(curr);
 
                 // first if two nodes are fully connected we put the one closer to inputs to next queue
                 if node_preceeds_current & current_preceeds_node {
-
                     match self.resolve_fully_connected_pair(curr, node) {
-
                         LayerAdjustment::MoveCurrent => {
-
                             next_queue.push_back(*curr);
                             continue 'bfs;
-
-                        }, LayerAdjustment::MovePartner => {
-
+                        }
+                        LayerAdjustment::MovePartner => {
                             next_queue.push_back(*node);
-                            continue 'bfs;
-
-                        }, LayerAdjustment::NoChanges => {}
+                            continue;
+                        }
+                        LayerAdjustment::NoChanges => {}
                     }
-
 
                 // if current node is needed for node in queue move curr to next
                 } else if current_preceeds_node & !node_preceeds_current {
@@ -368,28 +353,14 @@ impl Graph {
                 }
             }
 
-            // now we know our current node should be in the current layer. we can add it to the map
+            // we now know the current node belongs here
             rev_layers.insert(*curr, layer);
 
-            // now that we've ensured that the current node should be here and noted its layer we
-            // can set up the next step of the search
-            let predecessors = self.preds.get(&curr).unwrap();
-            for in_node in predecessors {
-                if !rev_layers.contains_key(in_node) {
-                    next_queue.push_back(*in_node);
-                }
-            }
-            
+            // add predecessors to next layer
+            next_queue = self.finalize_next_queue(next_queue, &curr, &rev_layers);
+
             // remove any node that have been placed in the next queue from the current queue
-            for shifted_node in &next_queue {
-
-                let remove_index = match queue.iter().position(|x| x == shifted_node) {
-                    Some(pos) => pos,
-                    None => continue,
-                };
-
-                queue.remove(remove_index).unwrap();
-            }
+            queue = Self::correct_queue(queue, &next_queue);
 
             // move on to next layer if ready
             if (queue.len() == 0) & (next_queue.len() > 0) {
@@ -405,27 +376,17 @@ impl Graph {
 
         // now we have a map with the layers completely backward
         // with layer == depth of network
-        
-        // sensors potentially in different layers or we might have pushed back internal nodes so
-        // far that they are in a sensor layer. lets just push all of the sensors to a new final
-        // reverse layer
-        for sensor_node in &self.sensors {
-            rev_layers.insert(*sensor_node, layer + 1);
-        }
-        
-        // now we can flip the reverse layers. We might be skipping layers in terms of the layer
-        // labels but that shouldn't matter once the sorting is done
-        let mut node_layers = HashMap::new();
-        for node in rev_layers.keys() {
-            // reverse the layer ordering
-            node_layers.insert(*node, layer + 1 - rev_layers[node]);
-        }
+
+        let node_layers = self.reverse_layer_map(rev_layers, layer);
 
         Ok(node_layers)
     }
 
-    fn resolve_fully_connected_pair(&self, current_node: &usize, check_node: &usize) -> LayerAdjustment {
-        
+    fn resolve_fully_connected_pair(
+        &self,
+        current_node: &usize,
+        check_node: &usize,
+    ) -> LayerAdjustment {
         let current_depth = match self.get_node_depth(current_node) {
             Ok(x) => x,
             Err(e) => panic!("Disconnected node! {}", e),
@@ -445,6 +406,60 @@ impl Graph {
             LayerAdjustment::NoChanges
         }
     }
+
+    fn finalize_next_queue(
+        &self,
+        mut tentative_queue: VecDeque<usize>,
+        current_node: &usize,
+        reverse_layers: &HashMap<usize, usize>,
+    ) -> VecDeque<usize> {
+        let predecessors = self.preds.get(current_node).unwrap();
+        for in_node in predecessors {
+            if !reverse_layers.contains_key(in_node) {
+                tentative_queue.push_back(*in_node);
+            }
+        }
+
+        tentative_queue.clone()
+    }
+
+    fn correct_queue(mut current_queue: VecDeque<usize>, next_layer_queue: &VecDeque<usize>) -> VecDeque<usize> {
+
+        for shifted_node in next_layer_queue {
+            let remove_index = match current_queue.iter().position(|x| x == shifted_node) {
+            Some(pos) => pos,
+            None => continue,
+            };
+        
+            current_queue.remove(remove_index).unwrap();
+        }
+
+    current_queue.clone()
+        
+    }
+
+    fn reverse_layer_map(
+        &self,
+        mut reverse_layers: HashMap<usize, usize>,
+        deepest_layer: usize,
+    ) -> HashMap<usize, usize> {
+        // sensors potentially in different layers or we might have pushed back internal nodes so
+        // far that they are in a sensor layer. lets just push all of the sensors to a new final
+        // reverse layer
+        for sensor_node in &self.sensors {
+            reverse_layers.insert(*sensor_node, deepest_layer + 1);
+        }
+
+        // now we can flip the reverse layers. We might be skipping layers in terms of the layer
+        // labels but that shouldn't matter once the sorting is done
+        let mut node_layers = HashMap::new();
+        for node in reverse_layers.keys() {
+            // reverse the layer ordering
+            node_layers.insert(*node, deepest_layer + 1 - reverse_layers[node]);
+        }
+
+        node_layers
+    }
 }
 
 #[cfg(test)]
@@ -459,7 +474,11 @@ mod tests {
 
     #[fixture]
     fn singly_recurrent_graph() -> Graph {
-        Graph::from_edge_list(vec![[0, 2], [1, 3], [3, 2], [3, 4], [2, 5], [4, 3], [4, 5]], vec![0, 1], vec![5])
+        Graph::from_edge_list(
+            vec![[0, 2], [1, 3], [3, 2], [3, 4], [2, 5], [4, 3], [4, 5]],
+            vec![0, 1],
+            vec![5],
+        )
     }
 
     #[fixture]
@@ -488,6 +507,75 @@ mod tests {
         Graph::from_edge_list(edges, vec![0, 1], vec![4])
     }
 
+    #[fixture]
+    fn singly_recurrent_layer_map_known() -> Vec<HashMap<usize, usize>> {
+        let mut known1: HashMap<usize, usize> = HashMap::new();
+        known1.insert(0, 0);
+        known1.insert(1, 0);
+        known1.insert(2, 2);
+        known1.insert(3, 1);
+        known1.insert(4, 2);
+        known1.insert(5, 3);
+
+        let mut known2: HashMap<usize, usize> = HashMap::new();
+        known2.insert(0, 0);
+        known2.insert(1, 0);
+        known2.insert(2, 3);
+        known2.insert(3, 2);
+        known2.insert(4, 3);
+        known2.insert(5, 4);
+
+
+        vec![known1, known2]
+    }
+    
+    #[fixture]
+    fn two_cycle_layer_map_known() -> Vec<HashMap<usize, usize>> {
+        let mut known1: HashMap<usize, usize> = HashMap::new();
+        known1.insert(0, 0);
+        known1.insert(1, 0);
+        known1.insert(2, 1);
+        known1.insert(3, 2);
+        known1.insert(4, 3);
+
+        let mut known2: HashMap<usize, usize> = HashMap::new();
+        known2.insert(0, 0);
+        known2.insert(1, 0);
+        known2.insert(2, 2);
+        known2.insert(3, 3);
+        known2.insert(4, 4);
+        
+        vec![known1, known2]
+    }
+
+    #[fixture]
+    fn three_cycle_layer_map_known() -> Vec<HashMap<usize, usize>> {
+
+        let mut known1: HashMap<usize, usize> = HashMap::new();
+        known1.insert(0, 0);
+        known1.insert(1, 0);
+        known1.insert(2, 1);
+        known1.insert(3, 1);
+        known1.insert(4, 2);
+        known1.insert(5, 3);
+        known1.insert(6, 4);
+        known1.insert(7, 5);
+        known1.insert(8, 5);
+        
+        let mut known2: HashMap<usize, usize> = HashMap::new();
+        known2.insert(0, 0);
+        known2.insert(1, 0);
+        known2.insert(2, 2);
+        known2.insert(3, 2);
+        known2.insert(4, 3);
+        known2.insert(5, 4);
+        known2.insert(6, 5);
+        known2.insert(7, 6);
+        known2.insert(8, 6);
+
+        vec![known1, known2]
+    }
+
     #[rstest]
     #[case(singly_recurrent_graph(), 2, 1)]
     #[case(three_cycle_graph(), 5, 3)]
@@ -507,40 +595,32 @@ mod tests {
         #[case] expected: usize,
         #[case] backward_flag: bool,
     ) {
-        assert_eq!(graph.get_path_length(&source, &target, backward_flag).unwrap(), expected)
+        assert_eq!(
+            graph
+                .get_path_length(&source, &target, backward_flag)
+                .unwrap(),
+            expected
+        )
     }
 
     #[rstest]
-    fn test_get_layer_map(three_cycle_graph: Graph) {
-        
-        // known values
-        let mut known: HashMap<usize, usize> = HashMap::new();
-        known.insert(0, 0);
-        known.insert(1, 0);
-        known.insert(2, 2);
-        known.insert(3, 2);
-        known.insert(4, 3);
-        known.insert(5, 4);
-        known.insert(6, 5);
-        known.insert(7, 6);
-        known.insert(8, 6);
+    #[case(singly_recurrent_graph(), singly_recurrent_layer_map_known())]
+    #[case(two_cycle_graph(), two_cycle_layer_map_known())]
+    #[case(three_cycle_graph(), three_cycle_layer_map_known())]
+    fn test_get_layer_map(#[case] graph: Graph, #[case] knowns: Vec<HashMap<usize, usize>>) {
+        let mut solution_in_possible_knowns = false;
+        let computed_layer_map = graph.get_layer_map().unwrap();
 
-        assert_eq!(known, three_cycle_graph.get_layer_map().unwrap())
-    }
+        println!("{:?}", computed_layer_map);
 
-    #[rstest]
-    fn test_get_layer_map_alt(singly_recurrent_graph: Graph) {
+        for known in knowns {
+            println!("known: {:?}", &known);
+            if known == computed_layer_map {
+                solution_in_possible_knowns = true;
+            }
+        }
 
-        // construct the known
-        let mut known: HashMap<usize, usize> = HashMap::new();
-        known.insert(0, 0);
-        known.insert(1, 0);
-        known.insert(2, 3);
-        known.insert(3, 2);
-        known.insert(4, 3);
-        known.insert(5, 4);
-
-        assert_eq!(known, singly_recurrent_graph.get_layer_map().unwrap())
+        assert!(solution_in_possible_knowns)
     }
 
     #[rstest]
@@ -549,11 +629,13 @@ mod tests {
                                           Ok(vec![0, 1, 3, 4, 2, 5]),
                                           Ok(vec![1, 0, 3, 4, 2, 5])])]
     #[case(two_cycle_graph(), vec![Ok(vec![0, 1, 2, 3, 4]), Ok(vec![1, 0, 2, 3, 4])])]
-    fn test_recurrent_pseudosort(#[case] graph: Graph, #[case] knowns: Vec<Result<Vec<usize>, ConnectivityError>>) {
-
+    fn test_recurrent_pseudosort(
+        #[case] graph: Graph,
+        #[case] knowns: Vec<Result<Vec<usize>, ConnectivityError>>,
+    ) {
         let mut solution_in_possible_knowns = false;
         let sort_result = graph.recurrent_pseudosort();
-        
+
         for known in knowns {
             println!("a known: {:?}", known);
             if known == sort_result {
@@ -563,5 +645,4 @@ mod tests {
         println!("{:?}", sort_result);
         assert!(solution_in_possible_knowns)
     }
-
 }
